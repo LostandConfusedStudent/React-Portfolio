63e5ae01cb665224efd132fca73b39cd14562291
pausun <pfsun9999@msn.com>
2020-12-11 17:43:07 -0800

    Working on styling and fixed Header and Footer components

--- develop/src/components/Footer.js
+++ develop/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 // Component for footer
@@ -8,21 +9,27 @@ function Footer() {
             <h1> </h1>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a className="nav-link" href="https://google.com">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="https://linkedin.com/in/paul-sun-87918598">Portfolio</a>
-                    </li>
+
+                    <div className="nav-link">
+                            <Link to="/Home">Home <span class="sr-only">(current)</span></Link>
+                        </div>
+
+                        <div className="nav-link">
+                            <Link to="/Portfolio">Portfolio <span class="sr-only">(current)</span></Link>
+                        </div> 
+
                     <li className="nav-item">
                         <a className="nav-link" href="mailto:pfsun9999@msn.com">Email</a>
                     </li>
+
                     <li className="nav-item">
                         <a className="nav-link" href="https://github.com/LostandConfusedStudent">GitHub</a>
                     </li>
+
                     <li className="nav-item">
                         <a className="nav-link" href="https://linkedin.com/in/paul-sun-87918598">LinkedIn</a>
                     </li>
+
                     <li className="nav-item">
                         <a className="nav-link" href="https://linkedin.com/in/paul-sun-87918598">Resume</a>
                     </li>
--- develop/src/components/Header.js
+++ develop/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../styles/Header.css";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Header() {
@@ -8,12 +9,15 @@ function Header() {
             <h1>Paul Sun</h1>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a className="nav-link" href="https://google.com">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="https://linkedin.com/in/paul-sun-87918598">Portfolio</a>
-                    </li>
+
+                        <div className="nav-link">
+                            <Link to="/Home">Home <span class="sr-only">(current)</span></Link>
+                        </div>
+
+                        <div className="nav-link">
+                            <Link to="/Portfolio">Portfolio <span class="sr-only">(current)</span></Link>
+                        </div> 
+
                     <li className="nav-item">
                         <a className="nav-link" href="mailto:pfsun9999@msn.com">Email</a>
                     </li>
